60b2294ca9d4a06721279182e12eaf28d7f7bbc8
drewosmond <drewosmond@gmail.com>
2022-07-23 15:04:48 -0700

    added basic react feature

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
+import "./App.css";
 
 function App() {
+  const [socket, setSocket] = useState<Socket | undefined>();
+  const [chatBox, setChatBox] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [messages, setMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const ioSocket = io("http://localhost:8040");
+    ioSocket.on("connect", () => {
+      setSocket(ioSocket);
+      setLoaded(true);
+    });
+
+    ioSocket.on("msg", (msg) => {
+      setMessages((prev) => {
+        return [...prev, msg];
+      });
+    });
+
+    return () => void ioSocket.close();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!loaded && <div>loading...</div>}
+      {loaded && socket?.emit && (
+        <>
+          <h1>Chat App :D</h1>
+          <input
+            type="text"
+            value={chatBox}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setChatBox("");
+                socket.emit("message", e.target.value);
+              }
+            }}
+            onChange={(e) => setChatBox(e.target.value)}
+          />
+        </>
+      )}
+      {messages.map((ele, i) => (
+        <div key={`text-${i}`}>{ele}</div>
+      ))}
+    </>
   );
 }
 
